bbf158654d00555896eb34dc4c11fa1db98fe598
Adby Santos <adby.santos15@outlook.com>
2023-08-13 17:32:37 -0300

    feat: resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/adby_santos_english_cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -27,8 +27,16 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Full CV
           </Button>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative", paddingTop: 40, fontSize: 20 }}>
+          Page One
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative", fontSize: 14 }}>
+          (download the full version above)
         </Row>
 
         <Row className="resume">
@@ -45,7 +53,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Download Full CV
           </Button>
         </Row>
       </Container>
